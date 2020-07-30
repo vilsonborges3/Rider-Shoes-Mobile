308199c009a4fe76afaffdb0ad2c8b4497303ad1
Vilson Borges <vilsonborges98@gmail.com>
2020-07-30 16:52:48 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+        <View>
+          <Text>
+            Welcome to react navtive
+          </Text>
+        </View>
+      );
+    };
+  }
+
