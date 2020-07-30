8302390ea7266c70b386da84b3500ee5b408f844
Vilson Borges <vilsonborges98@gmail.com>
2020-07-30 17:56:00 -0300

    setting editorConfig, eslint prettier and reactotron

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './config/reactotronConfig';
+
+import {
+  View,
+  Text,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View />
+  );
+}
